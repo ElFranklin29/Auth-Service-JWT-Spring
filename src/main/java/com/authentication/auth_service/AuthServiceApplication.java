a9d634437925ec83a1cff0044d795bc51847f095
Franklin <franklin2908david@gmail.com>
2025-05-08 18:03:34 -0500

    AGREGAR logica para manejar variables de entorno en railway

--- src/main/java/com/authentication/auth_service/AuthServiceApplication.java
+++ src/main/java/com/authentication/auth_service/AuthServiceApplication.java
@@ -8,10 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(entry -> {
-			System.setProperty(entry.getKey(), entry.getValue());
-		});
+		if (System.getenv("RAILWAY_ENVIRONMENT") == null) {
+			// Solo carga .env en desarrollo
+			Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+			dotenv.entries().forEach(entry ->
+					System.setProperty(entry.getKey(), entry.getValue())
+			);
+		}
 		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 
